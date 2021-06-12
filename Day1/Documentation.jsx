3f7a36ade7f41cc3021bbb4b332f1560f2481efa
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-06-12 22:53:59 +0530

    Create Documentation.jsx

---
+++ Day1/Documentation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Documentation() {
+    return (
+      <div className="App">
+       <h1>This is our new component</h1>
+       <button>This is a button</button>
+      </div>
+    );
+  }
+  
+  export default Documentation;
