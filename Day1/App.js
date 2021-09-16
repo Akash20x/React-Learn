ff718594bc4813cb844d2bc2f82f47c9fe4e82ce
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-09-16 20:45:29 +0530

    Update App.js

--- Day1/App.js
+++ Day1/App.js
@@ -1,19 +1,43 @@
-import React from "react";
 import logo from './logo.svg';
 import './App.css';
-import Documentation from "./Documentation";
+import React from 'react';
 
+// With jsx
+const text = <span>Hello React</span>;
+
+// Without jsx
+const elem = React.createElement('h2',{className:'elem'},'Hello H2 Tag');
+
+const test=() => {
+  alert("Hello Akash")
+}
+
+const age=function(a){
+  if(a>8){
+    return <span>You are eligible</span>
+   }else{
+     return <span>You are not eligible</span>     
+  }
+}
+
+// Function Component
 function App() {
-  const headerText="Hello Youtube from my variable";
-  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{headerText}</p>
-      </header>
+    <h1>Hello Akash,{text}</h1>
+     {elem}
+     <button onClick={test}></button>
+      {age(19)}
     </div>
   );
+}
+
+// Class Component
+class App extends React.Component
+{
+  render(){
+    return <h1>Hello, class component</h1>
+  }
 }
 
 export default App;
