d46f24610478366723a3ff986dfa47b3b25f821c
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-09-23 20:09:07 +0530

    Add files via upload

---
+++ Day7/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+// import Stylesheet from './components/Stylesheet';
+// import Inline from './components/Inline';
+// import './appstyle.css';
+// import Style from './appstyle.module.css';
+import Form from './components/Form';
+
+// Class Component
+class App extends React.Component
+{
+  render(){
+    return (
+      <div className="App">
+      {/* <h1 className={Style.success}>Green</h1>
+      <h1 className='error'>Error</h1>
+      <Stylesheet isvalue={false}></Stylesheet>
+      <Inline></Inline>     */}
+      <Form></Form>
+      </div>
+    );
+  }
+}
+
+export default App;
