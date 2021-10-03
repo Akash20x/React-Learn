1bb17f137463823c482e00050ac848470b949d58
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-10-03 21:19:45 +0530

    Add files via upload

---
+++ redux/Day1/Counter.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Counter = (props) => {
+    const {setCount,count} = props;
+
+//   const [count, setCount] = React.useState(0);
+
+  const increment = () => {
+      setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div style={{ backgroundColor: "grey", margin: "10px" }}>
+      {/* <h3> {`Count: ${count}`} </h3> */}
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button onClick={increment}> Increment </button>
+        <button onClick={decrement}> Decrement </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
