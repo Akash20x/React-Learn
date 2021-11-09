e1c65abf80d4301ff2d89f895b51da3a423528c9
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-11-09 22:42:12 +0530

    Add files via upload

---
+++ The Net Ninja/useEffect Cleanup/useFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCont = new AbortController(); // to abort the fetch when we fastly move to other page 
+        
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch(err => {
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
+          // auto catches network / connection error
+          setIsPending(false);
+          setError(err.message);
+        }
+      })
+    }, 1000);
+
+    // abort the fetch
+    return () => abortCont.abort();   // cleanup function for useEffect 
+                                      // called when there is loading in 1 page and you move to another page
+  }, [url])
+
+  return { data, isPending, error };  
+}
+ 
+export default useFetch;
