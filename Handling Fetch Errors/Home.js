1c7f1c0f1e8dfb6907b7827bce6f7bdb95070314
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-11-09 20:08:03 +0530

    Update Home.js

--- Handling Fetch Errors/Home.js
+++ Handling Fetch Errors/Home.js
@@ -1,30 +1,38 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
-// In this we added a loading variable and we check using isPending
-// setTimeout 1000 to add a extra time of 1 sec for the useEffect function
-// Loading is visible till isPending true
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
-      fetch('http://localhost:8001/blogs')
+      fetch('http://localhost:8000/blogs')
       .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
         return res.json();
       })
       .then(data => {
         setIsPending(false);
         setBlogs(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
       })
     }, 1000);
   }, [])
 
   return (
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} />}
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
